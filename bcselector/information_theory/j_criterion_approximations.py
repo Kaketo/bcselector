9551efeccb9ec6bc789bebd95c6ce62909a66233
Kaketo <33804998+Kaketo@users.noreply.github.com>
2020-03-25 09:41:43 +0100

    Add all approximations of J criterion

--- bcselector/information_theory/j_criterion_approximations.py
+++ bcselector/information_theory/j_criterion_approximations.py
@@ -36,17 +36,40 @@ def mim(data, target_variable, candidate_variable_index):
 
 def mifs(data, target_variable, prev_variables_index, candidate_variable_index, **kwargs):
     """
-    data - numpy matrix
-    target_variable (Y) - numpy array with target variable
-    prev_variables_index - index of previously selected variables in data matrix
-    candidate_variable_index (X_i) - index of candidate variable X_i in data
+    This estimator computes the Mutual Information Feature Selection criterion.
+    ----------
+    data : np.array matrix
+        Matrix of data set. Columns are variables, rows are observations.
+    target_variable : int or float
+        Target variable. Can not be in data!
+    prev_variables_index: list of ints
+        Indexes of previously selected variables.
+    candidate_variable_index : int
+        Index of candidate variable in data matrix.
+    beta: float
+        Impact of redundancy segment in MIFS approximation. Higher the beta is, higher the impact.
+    Returns
+    -------
+    j_criterion_value : float
+        J_criterion approximated by the Mutual Information Feature Selection.
     """
+    assert isinstance(data, np.ndarray), "Argument 'data' must be a numpy matrix"
+    assert isinstance(target_variable, np.ndarray), "Argument 'target_variable' must be a numpy matrix"
+    assert isinstance(candidate_variable_index, int), "Argument 'candidate_variable_index' must be an integer"
+
+    assert len(data.shape) == 2, "For 'data' argument use numpy array of shape (n,p)" 
+    assert data.shape[0] == len(target_variable), "Number of rows in 'data' must equal target_variable length"
+    assert candidate_variable_index < data.shape[1] - 1, "Index 'candidate_variable_index' out of range in 'data'"
+
+    for i in prev_variables_index:
+        assert isinstance(i, int), "All previous variable indexes must be int."
 
     if 'beta' not in kwargs.keys():
         beta = 1
         warnings.warn('Parameter \'beta\' not provided, default value of 1 is selected.')
     else:
         beta = kwargs.pop('beta')
+        assert isinstance(beta,int) or isinstance(beta,float), "Argument 'beta' must be int or float"
         assert len(kwargs) == 0, 'Unused parameters ' + str(list(kwargs.keys()))
 
     candidate_variable = data[:,candidate_variable_index]
@@ -59,12 +82,33 @@ def mifs(data, target_variable, prev_variables_index, candidate_variable_index, 
 
 def mrmr(data, target_variable, prev_variables_index, candidate_variable_index):
     """
-    data - numpy matrix
-    target_variable (Y) - numpy array with target variable
-    prev_variables_index - index of previously selected variables in data matrix
-    candidate_variable_index (X_i) - index of candidate variable X_i in data
+    This estimator computes the Max-Relevance Min-Redundancy criterion.
+    ----------
+    data : np.array matrix
+        Matrix of data set. Columns are variables, rows are observations.
+    target_variable : int or float
+        Target variable. Can not be in data!
+    prev_variables_index: list of ints
+        Indexes of previously selected variables.
+    candidate_variable_index : int
+        Index of candidate variable in data matrix.
+    Returns
+    -------
+    j_criterion_value : float
+        J_criterion approximated by the Max-Relevance Min-Redundancy.
     """
     
+    assert isinstance(data, np.ndarray), "Argument 'data' must be a numpy matrix"
+    assert isinstance(target_variable, np.ndarray), "Argument 'target_variable' must be a numpy matrix"
+    assert isinstance(candidate_variable_index, int), "Argument 'candidate_variable_index' must be an integer"
+
+    assert len(data.shape) == 2, "For 'data' argument use numpy array of shape (n,p)" 
+    assert data.shape[0] == len(target_variable), "Number of rows in 'data' must equal target_variable length"
+    assert candidate_variable_index < data.shape[1] - 1, "Index 'candidate_variable_index' out of range in 'data'"
+
+    for i in prev_variables_index:
+        assert isinstance(i, int), "All previous variable indexes must be int."
+
     candidate_variable = data[:,candidate_variable_index]
     prev_variables_len = 1 if len(prev_variables_index) == 0 else len(prev_variables_index)
     
@@ -77,12 +121,32 @@ def mrmr(data, target_variable, prev_variables_index, candidate_variable_index):
 
 def jmi(data, target_variable, prev_variables_index, candidate_variable_index):
     """
-    data - numpy matrix
-    target_variable (Y) - numpy array with target variable
-    prev_variables_index - index of previously selected variables in data matrix
-    candidate_variable_index (X_i) - index of candidate variable X_i in data
+    This estimator computes the Joint Mutual Information criterion.
+    ----------
+    data : np.array matrix
+        Matrix of data set. Columns are variables, rows are observations.
+    target_variable : int or float
+        Target variable. Can not be in data!
+    prev_variables_index: list of ints
+        Indexes of previously selected variables.
+    candidate_variable_index : int
+        Index of candidate variable in data matrix.
+    Returns
+    -------
+    j_criterion_value : float
+        J_criterion approximated by the Joint Mutual Information.
     """
+    
+    assert isinstance(data, np.ndarray), "Argument 'data' must be a numpy matrix"
+    assert isinstance(target_variable, np.ndarray), "Argument 'target_variable' must be a numpy matrix"
+    assert isinstance(candidate_variable_index, int), "Argument 'candidate_variable_index' must be an integer"
 
+    assert len(data.shape) == 2, "For 'data' argument use numpy array of shape (n,p)" 
+    assert data.shape[0] == len(target_variable), "Number of rows in 'data' must equal target_variable length"
+    assert candidate_variable_index < data.shape[1] - 1, "Index 'candidate_variable_index' out of range in 'data'"
+
+    for i in prev_variables_index:
+        assert isinstance(i, int), "All previous variable indexes must be int."
     candidate_variable = data[:,candidate_variable_index]
     prev_variables_len = 1 if len(prev_variables_index) == 0 else len(prev_variables_index)
 
@@ -97,17 +161,40 @@ def jmi(data, target_variable, prev_variables_index, candidate_variable_index):
 
 def cife(data, target_variable, prev_variables_index, candidate_variable_index, **kwargs):
     """
-    data - numpy matrix
-    target_variable (Y) - numpy array with target variable
-    prev_variables_index - index of previously selected variables in data matrix
-    candidate_variable_index (X_i) - index of candidate variable X_i in data
+    This estimator computes the Conditional Infomax Feature Extraction criterion.
+    ----------
+    data : np.array matrix
+        Matrix of data set. Columns are variables, rows are observations.
+    target_variable : int or float
+        Target variable. Can not be in data!
+    prev_variables_index: list of ints
+        Indexes of previously selected variables.
+    candidate_variable_index : int
+        Index of candidate variable in data matrix.
+    beta: float
+        Impact of redundancy segment in MIFS approximation. Higher the beta is, higher the impact.
+    Returns
+    -------
+    j_criterion_value : float
+        J_criterion approximated by the Conditional Infomax Feature Extraction.
     """
+    assert isinstance(data, np.ndarray), "Argument 'data' must be a numpy matrix"
+    assert isinstance(target_variable, np.ndarray), "Argument 'target_variable' must be a numpy matrix"
+    assert isinstance(candidate_variable_index, int), "Argument 'candidate_variable_index' must be an integer"
+
+    assert len(data.shape) == 2, "For 'data' argument use numpy array of shape (n,p)" 
+    assert data.shape[0] == len(target_variable), "Number of rows in 'data' must equal target_variable length"
+    assert candidate_variable_index < data.shape[1] - 1, "Index 'candidate_variable_index' out of range in 'data'"
+
+    for i in prev_variables_index:
+        assert isinstance(i, int), "All previous variable indexes must be int."
 
     if 'beta' not in kwargs.keys():
         beta = 1
         warnings.warn('Parameter \'beta\' not provided, default value of 1 is selected.')
     else:
         beta = kwargs.pop('beta')
+        assert isinstance(beta,int) or isinstance(beta,float), "Argument 'beta' must be int or float"
         assert len(kwargs) == 0, 'Unused parameters ' + str(list(kwargs.keys()))
 
     candidate_variable = data[:,candidate_variable_index]
--- bcselector/tests/j_criterion_approximations_test.py
+++ bcselector/tests/j_criterion_approximations_test.py
@@ -7,17 +7,6 @@ from bcselector.information_theory.basic_approximations import mutual_informatio
 
 
 class TestMIM(unittest.TestCase):
-    # def __init__(self):
-        
-    #     self.empty_matrix = np.array([[],[]])
-    #     self.zeros_matrix = np.zeros((100,50))
-    #     self.integer_matrix = np.random.randint(0,10,(100,50))
-    #     self.float_matrix = np.random.normal(0,1,(100,50))
-
-    #     self.diverse_target = np.random.randint(0,10,(100))
-    #     self.non_diverse_target = np.random.binomial(1,0.1,100)
-    #     self.one_class_target = np.repeat(1,100)
-    
     def test_empty_imput(self):
         empty_matrix = np.array([[],[]])
         diverse_target = np.random.randint(0,10,(100))
@@ -34,3 +23,41 @@ class TestMIM(unittest.TestCase):
         diverse_target = np.random.randint(0,10,(100))
         candidate_index = 1
         self.assertAlmostEqual(mim(integer_matrix, diverse_target, candidate_index), mutual_information(diverse_target,integer_matrix[:,candidate_index]), places = 5)
+
+    def test_one_class_target(self):
+        integer_matrix = np.random.randint(0,10,(100,50))
+        one_class_target = np.repeat(1,100)
+        candidate_index = 1
+        self.assertAlmostEqual(mim(integer_matrix, one_class_target, candidate_index), 0.0, places = 5)
+
+class TestMIFS(unittest.TestCase):
+    def test_empty_imput(self):
+        empty_matrix = np.array([[],[]])
+        diverse_target = np.random.randint(0,10,(100))
+        prev_variable_index = [3,4,5]
+        candidate_index = 1
+        with self.assertRaises(AssertionError): mifs(empty_matrix, diverse_target, prev_variable_index, candidate_index, beta = 10)
+
+    def test_no_beta_provided(self):
+        zeros_matrix = np.zeros((100,50))
+        diverse_target = np.random.randint(0,10,(100))
+        prev_variable_index = [3,4,5]
+        candidate_index = 1
+        self.assertIsInstance(mifs(zeros_matrix, diverse_target, prev_variable_index, candidate_index),float)
+
+    def test_too_high_candidate_index(self):
+        zeros_matrix = np.zeros((100,50))
+        diverse_target = np.random.randint(0,10,(100))
+        prev_variable_index = [3,4,5]
+        too_high_index = 51
+        with self.assertRaises(AssertionError): mifs(zeros_matrix, diverse_target, prev_variable_index, too_high_index)    
+
+    def test_theoretical_value(self):
+        integer_matrix = np.random.randint(0,10,(100,50))
+        diverse_target = np.random.randint(0,10,(100))
+        prev_variable_index = [3,4,5]
+        candidate_index = 1
+        beta_1 = 1
+        beta_2 = 10
+        self.assertGreater(mifs(integer_matrix,diverse_target,prev_variable_index,candidate_index,beta = beta_1), 
+                            mifs(integer_matrix,diverse_target,prev_variable_index,candidate_index,beta = beta_2))
