5344dd3f3ab5a678f3bbabf42fc6dbfee3edd5dc
Kaketo <33804998+Kaketo@users.noreply.github.com>
2020-03-25 11:33:48 +0100

    Add no cost based filter method + tests

---
+++ bcselector/filter_methods/no_cost_based_filter_methods.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def no_cost_find_best_feature(j_criterion_func, r, data, target_variable, possible_variables_index, costs, **kwargs):
+    # FUnkcja dokonująca selekcji bez uwzględnienia kosztów
+    variables_result = []
+    for i in possible_variables_index:
+        variables_result.append(j_criterion_func(data, 
+                                                target_variable = target_variable, 
+                                                candidate_variable_index=i,
+                                                **kwargs))
+    k = np.argmax(variables_result)
+    return possible_variables_index[k], variables_result[k], costs[k]
