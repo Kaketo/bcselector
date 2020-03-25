c175043580c651c6f16c00255e22708feaa7059f
Kaketo <33804998+Kaketo@users.noreply.github.com>
2020-03-25 22:16:19 +0100

    Add FractionVariableSelector

--- bcselector/tests/variable_selection_test.py
+++ bcselector/tests/variable_selection_test.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 import pandas as pd
 
-from bcselector.variable_selection import DiffVariableSelector
+from bcselector.variable_selection import DiffVariableSelector, FractionVariableSelector
 
 class TestDiffVariableSelector(unittest.TestCase):
     def test_numpy_input(self):
@@ -51,5 +51,52 @@ class TestDiffVariableSelector(unittest.TestCase):
                 j_criterion_func='mim')
         
         self.assertEqual(dvs.variables_selected_order[0], 2)
+
+class TestFractionVariableSelector(unittest.TestCase):
+    def test_numpy_input(self):
+        integer_matrix = np.random.randint(0,10,(100,10))
+        diverse_target = np.random.randint(0,10,(100))
+        costs = [ 1.76,  0.19, -0.36,  0.96,  0.41,  0.17, -0.36,  0.75,  0.79, -1.38]
+        r = 1
+
+        fvs = FractionVariableSelector()
+        fvs.fit(data=integer_matrix,
+                target_variable=diverse_target,
+                costs=costs,
+                r=r,
+                j_criterion_func='mim')
+
+        self.assertIsInstance(fvs.variables_selected_order, list)
+        self.assertEqual(len(fvs.variables_selected_order), len(costs))
+
+    def test_pandas_input(self):
+        integer_matrix = pd.DataFrame(np.random.randint(0,10,(100,3)), columns=['AA','BB','CC'])
+        diverse_target = np.random.randint(0,2,(100))
+        costs = {'AA': 10, 'BB': 1, 'CC': 1.5}
+        r = 1
+        fvs = FractionVariableSelector()
+        fvs.fit(data=integer_matrix,
+                target_variable=diverse_target,
+                costs=costs,
+                r=r,
+                j_criterion_func='mim')
+
+        self.assertIsInstance(fvs.variables_selected_order, list)
+        self.assertEqual(len(fvs.variables_selected_order), len(costs))
+
+    def test_theoretical_output(self):
+        integer_matrix = np.array([[0,1,0],[0,1,0],[0,1,2],[0,1,3],[1,1,5]])
+        diverse_target = np.array([0,0,0,0,1])
+        costs = [1,1,1]
+        r = 1
+
+        fvs = FractionVariableSelector()
+        fvs.fit(data=integer_matrix,
+                target_variable=diverse_target,
+                costs=costs,
+                r=r,
+                j_criterion_func='mim')
+        
+        self.assertEqual(fvs.variables_selected_order[0], 2)
 
 
--- bcselector/variable_selection.py
+++ bcselector/variable_selection.py
@@ -19,7 +19,7 @@ class _MockVariableSelector():
         self.cost_variables_selected_order = None
         self.j_criterion_func = None
 
-    def fit(self, data, target_variable, costs, lamb, j_criterion_func = 'cife', **kwargs):
+    def fit(self, data, target_variable, costs, j_criterion_func = 'cife', **kwargs):
         # data & costs
         assert isinstance(data, np.ndarray) or isinstance(data, pd.DataFrame), "Argument `data` must be numpy.ndarray or pandas.DataFrame"
         if isinstance(data,np.ndarray):
@@ -49,10 +49,6 @@ class _MockVariableSelector():
             self.target_variable = target_variable
         
         assert self.data.shape[0] == len(self.target_variable), "Number of rows in 'data' must equal target_variable length"
-
-        # lamb
-        assert isinstance(lamb, int) or isinstance(lamb, float), "Argument `lamb` must be integer or float"
-        self.lamb = lamb
 
         # j_criterion_func
         j_criterion_dict = {'mim':mim,'mifs':mifs,'mrmr':mrmr,'jmi':jmi,'cife':cife}
@@ -72,75 +68,12 @@ class DiffVariableSelector(_MockVariableSelector):
     --------
 
     """
-    def __init__(self):
-        self.data = None
-        self.target_variable = None
-        self.costs = None
-
-        self.variables_selected_order = None
-        self.cost_variables_selected_order = None
-        self.j_criterion_func = None
-
     def fit(self, data, target_variable, costs, lamb, j_criterion_func = 'cife', **kwargs):
-        """Ranks all features in dataset with difference cost filter method.
-
-        Parameters
-        ----------
-        data: np.array, pd.DataFrame
-            Data to be ranked, can be numpy matrix or pandas DataFrame.
-        costs : list, dict
-            Costs of all varibles provided in `data`.
-        lamb : float, inr
-        
-        j_criterion_func : function
-
-
-        Attributes
-        ----------
-
-        Examples
-        --------
-
-        """
-        # data & costs
-        assert isinstance(data, np.ndarray) or isinstance(data, pd.DataFrame), "Argument `data` must be numpy.ndarray or pandas.DataFrame"
-        if isinstance(data,np.ndarray):
-            assert isinstance(costs,list), "When using `data` as np.array, provide `costs` as list of floats or integers"
-        else:
-            assert isinstance(costs,(list, dict)), "When using `data` as pd.DataFrame, provide `costs` as list of floats or integers or dict {'col_1':cost_1,...}"
-
-        if isinstance(data, pd.DataFrame):
-            self.data = data.values
-            if isinstance(costs,dict):
-                self.costs = [costs[x] for x in data.columns]
-            else:
-                self.costs = costs
-        else:
-            self.data = data
-            self.costs = costs
-
-        assert len(self.data.shape) == 2, "For `data` argument use numpy array of shape (n,p) or pandas DataFrame" 
-        assert data.shape[1] == len(costs), "Length od cost must equal number of columns in `data`"
-
-        # target_variable
-        assert isinstance(target_variable,np.ndarray) or isinstance(target_variable,pd.core.series.Series), "Use np.array or pd.Series for argument `target_variable`"
-
-        if isinstance(target_variable,pd.core.series.Series):
-            self.target_variable = target_variable.values
-        else:
-            self.target_variable = target_variable
-        
-        assert self.data.shape[0] == len(self.target_variable), "Number of rows in 'data' must equal target_variable length"
-
         # lamb
         assert isinstance(lamb, int) or isinstance(lamb, float), "Argument `lamb` must be integer or float"
         self.lamb = lamb
-
-        # j_criterion_func
-        j_criterion_dict = {'mim':mim,'mifs':mifs,'mrmr':mrmr,'jmi':jmi,'cife':cife}
-        assert j_criterion_func in ['mim','mifs','mrmr','jmi','cife'], "Argument `j_criterion_func` must be one of ['mim','mifs','mrmr','jmi','cife']"
-        self.j_criterion_func = j_criterion_dict[j_criterion_func]
-
+        super().fit(data, target_variable, costs, j_criterion_func, **kwargs)
+        
         S = set()
         U = set([i for i in range(self.data.shape[1])])
 
@@ -155,6 +88,45 @@ class DiffVariableSelector(_MockVariableSelector):
                                 possible_variables_index = list(U),
                                 costs = self.costs,
                                 lamb = self.lamb)
+            S.add(k)
+            self.variables_selected_order.append(k)
+            self.cost_variables_selected_order.append(cost)
+            U = U.difference(set([k]))
+
+class FractionVariableSelector(_MockVariableSelector):
+    """Ranks all features in dataset with difference cost filter method.
+
+    Parameters
+    ----------
+
+    Attributes
+    ----------
+
+    Examples
+    --------
+
+    """
+    def fit(self, data, target_variable, costs, r, j_criterion_func = 'cife', **kwargs):
+        # lamb
+        assert isinstance(r, int) or isinstance(r, float), "Argument `lamb` must be integer or float"
+        self.r = r
+
+        super().fit(data, target_variable, costs, j_criterion_func, **kwargs)
+        
+        S = set()
+        U = set([i for i in range(self.data.shape[1])])
+
+        self.variables_selected_order = []
+        self.cost_variables_selected_order = []
+
+        while len(U) > 0:
+            k, _, cost = fraction_find_best_feature(j_criterion_func = self.j_criterion_func, 
+                                data = self.data, 
+                                target_variable = self.target_variable, 
+                                prev_variables_index = list(S),
+                                possible_variables_index = list(U),
+                                costs = self.costs,
+                                r = self.r)
             S.add(k)
             self.variables_selected_order.append(k)
             self.cost_variables_selected_order.append(cost)
