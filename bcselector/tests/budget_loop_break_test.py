651ff77a1baed387c8ff1d623f1e5c60600e3e6c
Kaketo <tomaszklonecki@gmail.com>
2020-10-05 23:24:12 +0200

    @add testing breaking loop woth budget

---
+++ bcselector/tests/budget_loop_break_test.py
@@ -0,0 +1,39 @@
+import unittest
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+from bcselector.variable_selection import DiffVariableSelector, FractionVariableSelector, NoCostVariableSelector
+from bcselector.data_generation import MatrixGenerator, DataFrameGenerator
+
+class TestMatrixGenerator(unittest.TestCase):
+    def test_cife(self):
+        # Given
+        n_cols = 10
+        n_rows = 100
+        model = LogisticRegression()
+
+        # When
+        mg = MatrixGenerator()
+        X, y, costs = mg.generate(n_rows=n_rows, n_basic_cols=n_cols, seed=2)
+        lamb = 1
+        beta = 0.5
+
+        dvs = DiffVariableSelector()
+        dvs.fit(data=X,
+                target_variable=y,
+                costs=costs,
+                lamb=lamb,
+                j_criterion_func='cife',
+                budget = 15, 
+                stop_budget=True,
+                beta=beta)
+        dvs.scoreCV(model=model, cv = 5)
+        dvs.plot_scores(compare_no_cost_method=True, model=model)
+
+        # Then
+        self.assertGreater(len(costs), len(dvs.variables_selected_order))
+
+
+if __name__ == '__main__':
+    unittest.main()   
